14e4c21af9827227b5a9c2f957ed116cf1bf8514
JDStraughan <jdstraughan@gmail.com>
2013-02-18 15:42:33 -0600

    Show the score

--- game.js
+++ game.js
@@ -35,6 +35,13 @@ game = {
     context.fill();
   },
   
+  drawScore: function() {
+    context.fillStyle = '#999';
+    context.font = (canvas.height) + 'px Impact, sans-serif';
+    context.textAlign = 'center';
+    context.fillText(game.score, canvas.width/2, canvas.height  * .9);
+  },
+  
   resetCanvas: function() {
     context.clearRect(0, 0, canvas.width, canvas.height);
   }
@@ -184,6 +191,7 @@ var requestAnimationFrame =
 function loop() {
   if (game.over == false) {
     game.resetCanvas();
+    game.drawScore();
     snake.move();
     food.draw();
     snake.draw();    
